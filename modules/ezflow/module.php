9b7c7ee841e459b9451ab743f977504ca4f0ac1d
Lukasz Serwatka <ls@ez.no>
2007-11-19 12:58:45 +0000

    - Moved to ezflow.
    git-svn-id: http://svn.ez.no/svn/extensions/ezflow/trunk@2192 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- modules/ezflow/module.php
+++ modules/ezflow/module.php
@@ -2,9 +2,26 @@
 
 $Module = array( 'name' => 'eZ Flow' );
 
+$ViewList = array();
 $ViewList['timeline'] = array( 'script' => 'timeline.php',
                                'params' => array( 'NodeID', 'LanguageCode' ) );
 
 $ViewList['preview'] = array( 'script' => 'preview.php',
                               'params' => array( 'Time', 'NodeID' ) );
+
+$ViewList['zone'] = array(
+                            'script' => 'zone.php',
+                            'params' => array( 'ContentObjectAttributeID', 'Version', 'ZoneID' )
+                         );
+
+$ViewList['request'] = array(
+                            'script' => 'request.php',
+                            'unordered_params' => array( 'items' => 'Items',
+                                                         'block' => 'Block' )
+                         );
+
+$ViewList['block'] = array(
+                            'script' => 'block.php',
+                            'params' => array( 'BlockID', 'Output' )
+                         );
 ?>
