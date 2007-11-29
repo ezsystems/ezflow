c8c807b5f32ceabcd1140201fc6571c0cde0be55
Lukasz Serwatka <ls@ez.no>
2007-11-29 18:09:27 +0000

    - Saved work.
    git-svn-id: http://svn.ez.no/svn/extensions/ezflow/trunk@2298 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- settings/zone.ini.append.php
+++ settings/zone.ini.append.php
@@ -4,8 +4,8 @@
 AllowedTypes[]=GlobalZoneLayout
 AllowedTypes[]=2ZonesLayout1
 AllowedTypes[]=2ZonesLayout2
+AllowedTypes[]=3ZonesLayout1
 AllowedTypes[]=3ZonesLayout2
-AllowedTypes[]=3ZonesLayout3
 AllowedTypes[]=4ZonesLayout1
 
 [GlobalZoneLayout]
@@ -45,6 +45,21 @@ ZoneThumbnail=2zones_layout2.gif
 
 Template=2zoneslayout2.tpl
 
+[3ZonesLayout1]
+ZoneTypeName=3 zones (layout 1)
+
+Zones[]=left
+Zones[]=right
+Zones[]=bottom
+
+ZoneName[left]=Left zone
+ZoneName[right]=Right zone
+ZoneName[bottom]=Bottom zone
+
+ZoneThumbnail=3zones_layout1.gif
+
+Template=3zoneslayout1.tpl
+
 [3ZonesLayout2]
 ZoneTypeName=3 zones (layout 2)
 
@@ -60,33 +75,18 @@ ZoneThumbnail=3zones_layout2.gif
 
 Template=3zoneslayout2.tpl
 
-[3ZonesLayout3]
-ZoneTypeName=3 zones (layout 3)
-
-Zones[]=left
-Zones[]=right
-Zones[]=bottom
-
-ZoneName[left]=Left zone
-ZoneName[right]=Right zone
-ZoneName[bottom]=Bottom zone
-
-ZoneThumbnail=3zones_layout3.gif
-
-Template=3zoneslayout3.tpl
-
 [4ZonesLayout1]
 ZoneTypeName=4 zones (layout 1)
 
 Zones[]=left
-Zones[]=middle
 Zones[]=right
-Zones[]=bottom
+Zones[]=bottomleft
+Zones[]=bottomright
 
 ZoneName[left]=Left zone
-ZoneName[middle]=Middle zone
 ZoneName[right]=Right zone
-ZoneName[right]=Bottom zone
+ZoneName[bottomleft]=Bottom left zone
+ZoneName[bottomright]=Bottom right zone
 
 ZoneThumbnail=4zones_layout1.gif
 
