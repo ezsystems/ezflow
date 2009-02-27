ea6d8a84254074ef81a493adbd2accddf64e1aaa
Lukasz Serwatka <ls@ez.no>
2009-02-27 14:58:40 +0000

    - Updated democontent.sql - Updated PostgreSQL table def
    git-svn-id: http://svn.ez.no/svn/extensions/ezflow/trunk@3650 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezflow_extension/ezextension/ezflow/sql/postgresql/postgresql.sql
+++ packages/ezflow_extension/ezextension/ezflow/sql/postgresql/postgresql.sql
@@ -1,11 +1,11 @@
 CREATE TABLE ezm_block (
-    id CHAR(32) NOT NULL,
-    zone_id CHAR(32) NOT NULL,
-    name CHARACTER VARYING(255) NULL,
+    id VARCHAR(32) NOT NULL,
+    zone_id VARCHAR(32) NOT NULL,
+    name VARCHAR(255) NULL,
     node_id INTEGER NOT NULL,
-    overflow_id CHAR(32) NULL,
+    overflow_id VARCHAR(32) NULL,
     last_update INTEGER NULL DEFAULT 0,
-    block_type CHARACTER VARYING(255) NULL,
+    block_type VARCHAR(255) NULL,
     fetch_params TEXT NULL,
     rotation_type INTEGER NULL,
     rotation_interval INTEGER NULL,
@@ -18,7 +18,7 @@ CREATE INDEX ezm_block_node_id ON ezm_block USING btree (node_id);
 ALTER TABLE ONLY ezm_block ADD CONSTRAINT ezm_block_pkey PRIMARY KEY (id);
 
 CREATE TABLE ezm_pool (
-    block_id CHAR(32) NOT NULL,
+    block_id VARCHAR(32) NOT NULL,
     object_id INTEGER NOT NULL,
     node_id INTEGER NOT NULL,
     priority INTEGER NULL DEFAULT 0,
@@ -26,7 +26,7 @@ CREATE TABLE ezm_pool (
     ts_visible INTEGER NULL DEFAULT 0,
     ts_hidden INTEGER NULL DEFAULT 0,
     rotation_until INTEGER NULL DEFAULT 0,
-    moved_to CHAR(32) NULL
+    moved_to VARCHAR(32) NULL
 );
 
 CREATE INDEX ezm_pool_block_id_ts_publication_priority ON ezm_pool USING btree (block_id,ts_publication,priority);
