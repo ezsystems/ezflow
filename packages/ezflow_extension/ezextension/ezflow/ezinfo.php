41caea13e23b63f133135a8e28d3ad0486a38996
Lukasz Serwatka <ls@ez.no>
2009-03-10 08:32:30 +0000

    - Changed version to RC1
    git-svn-id: http://svn.ez.no/svn/extensions/ezflow/trunk@3682 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezflow_extension/ezextension/ezflow/ezinfo.php
+++ packages/ezflow_extension/ezextension/ezflow/ezinfo.php
@@ -29,7 +29,7 @@ class ezflowInfo
     static function info()
     {
         return array( 'Name' => 'eZ Flow',
-                      'Version' => '1.1.0beta1',
+                      'Version' => '1.1-0rc1',
                       'Copyright' => 'Copyright (C) 1999-' . date('Y') . ' eZ Systems AS',
                       'License' => 'GNU General Public License v2.0'
                      );
