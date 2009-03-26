5a8bc1f132eff4426ec18a1b1c90727bf5ceeb8c
Lukasz Serwatka <ls@ez.no>
2009-03-26 13:35:58 +0000

    - Updated jpn-JP translation
    git-svn-id: http://svn.ez.no/svn/extensions/ezflow/trunk@3771 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezflow_extension/ezextension/ezflow/translations/jpn-JP/translation.ts
+++ packages/ezflow_extension/ezextension/ezflow/translations/jpn-JP/translation.ts
@@ -60,348 +60,348 @@
     </message>
     <message>
         <source>Search phrase</source>
-        <translation type="unfinished"></translation>
+        <translation>検索フレーズ</translation>
     </message>
     <message>
         <source>Section to search</source>
-        <translation type="unfinished"></translation>
+        <translation>検索対象セクション</translation>
     </message>
     <message>
         <source>Date range</source>
-        <translation type="unfinished"></translation>
+        <translation>対象期間</translation>
     </message>
     <message>
         <source>Past day</source>
-        <translation type="unfinished"></translation>
+        <translation>24時間以内</translation>
     </message>
     <message>
         <source>Past week</source>
-        <translation type="unfinished"></translation>
+        <translation>過去一週間</translation>
     </message>
     <message>
         <source>Past month</source>
-        <translation type="unfinished"></translation>
+        <translation>過去一ヶ月</translation>
     </message>
     <message>
         <source>Past 3 months</source>
-        <translation type="unfinished"></translation>
+        <translation>過去三ヶ月</translation>
     </message>
     <message>
         <source>Past year</source>
-        <translation type="unfinished"></translation>
+        <translation>過去一年</translation>
     </message>
     <message>
         <source>Select:</source>
-        <translation type="unfinished"></translation>
+        <translation>選択: </translation>
     </message>
 </context>
 <context>
     <name>design/ezflow/edit/frontpage</name>
     <message>
         <source>Search phrase</source>
-        <translation type="unfinished"></translation>
+        <translation>検索フレーズ</translation>
     </message>
     <message>
         <source>Search</source>
-        <translation type="unfinished">検索</translation>
+        <translation>検索</translation>
     </message>
     <message>
         <source>Section to search</source>
-        <translation type="unfinished"></translation>
+        <translation>検索対象セクション</translation>
     </message>
     <message>
         <source>Date range</source>
-        <translation type="unfinished"></translation>
+        <translation>対象期間</translation>
     </message>
     <message>
         <source>Past day</source>
-        <translation type="unfinished"></translation>
+        <translation>24時間以内</translation>
     </message>
     <message>
         <source>Past week</source>
-        <translation type="unfinished"></translation>
+        <translation>過去一週間</translation>
     </message>
     <message>
         <source>Past month</source>
-        <translation type="unfinished"></translation>
+        <translation>過去一ヶ月</translation>
     </message>
     <message>
         <source>Past 3 months</source>
-        <translation type="unfinished"></translation>
+        <translation>過去三ヶ月</translation>
     </message>
     <message>
         <source>Past year</source>
-        <translation type="unfinished"></translation>
+        <translation>過去一年</translation>
     </message>
     <message>
         <source>Results</source>
-        <translation type="unfinished">結果</translation>
+        <translation>結果</translation>
     </message>
     <message>
         <source>Select:</source>
-        <translation type="unfinished"></translation>
+        <translation>選択: </translation>
     </message>
     <message>
         <source>Add to block</source>
-        <translation type="unfinished">ブロックへ追加</translation>
+        <translation>ブロックへ追加</translation>
     </message>
     <message>
         <source>Edit %1 - %2</source>
-        <translation type="unfinished">%1 - %2を編集する</translation>
+        <translation>%1 - %2を編集する</translation>
     </message>
     <message>
         <source>Translating content from %from_lang to %to_lang</source>
-        <translation type="unfinished">%from_langから%to_langにコンテンツを翻訳する</translation>
+        <translation>%from_langから%to_langにコンテンツを翻訳する</translation>
     </message>
     <message>
         <source>Content in %language</source>
-        <translation type="unfinished">%languageのコンテンツ</translation>
+        <translation>%languageのコンテンツ</translation>
     </message>
     <message>
         <source>Send for publishing</source>
-        <translation type="unfinished">送信して公開</translation>
+        <translation>送信して公開</translation>
     </message>
     <message>
         <source>Store draft</source>
-        <translation type="unfinished">下書きの保存</translation>
+        <translation>下書きの保存</translation>
     </message>
     <message>
         <source>Discard draft</source>
-        <translation type="unfinished">下書きの破棄</translation>
+        <translation>下書きの破棄</translation>
     </message>
 </context>
 <context>
     <name>design/ezflow/embed/poll</name>
     <message>
         <source>Vote</source>
-        <translation type="unfinished">投票する</translation>
+        <translation>投票する</translation>
     </message>
 </context>
 <context>
     <name>design/ezflow/pagelayout</name>
     <message>
         <source>Skip to main content</source>
-        <translation type="unfinished"></translation>
+        <translation>メインコンテンツへスキップする</translation>
     </message>
 </context>
 <context>
     <name>design/ezflow/parts/website_toolbar</name>
     <message>
         <source>Timeline</source>
-        <translation type="unfinished">タイムライン</translation>
+        <translation>タイムライン</translation>
     </message>
 </context>
 <context>
     <name>design/iphone/collectedinfo/poll</name>
     <message>
         <source>Poll %pollname</source>
-        <translation type="unfinished">投票: %pollname</translation>
+        <translation>投票: %pollname</translation>
     </message>
     <message>
         <source>Results</source>
-        <translation type="unfinished">結果</translation>
+        <translation>結果</translation>
     </message>
     <message>
         <source>Please log in to vote on this poll.</source>
-        <translation type="unfinished">この投票に投票するにはログインしてください。</translation>
+        <translation>この投票に投票するにはログインしてください。</translation>
     </message>
     <message>
         <source>You have already voted for this poll.</source>
-        <translation type="unfinished">この投票にすでに投票しています。</translation>
+        <translation>この投票にすでに投票しています。</translation>
     </message>
     <message>
         <source>Votes</source>
-        <translation type="unfinished">票</translation>
+        <translation>票</translation>
     </message>
     <message>
         <source>%count total votes</source>
-        <translation type="unfinished">合計票: %count</translation>
+        <translation>合計票: %count</translation>
     </message>
     <message>
         <source>Back to poll</source>
-        <translation type="unfinished">投票に戻る</translation>
+        <translation>投票に戻る</translation>
     </message>
 </context>
 <context>
     <name>design/iphone/embed/poll</name>
     <message>
         <source>Vote</source>
-        <translation type="unfinished">投票する</translation>
+        <translation>投票する</translation>
     </message>
 </context>
 <context>
     <name>design/iphone/full/image</name>
     <message>
         <source>Previous</source>
-        <translation type="unfinished">前へ</translation>
+        <translation>前へ</translation>
     </message>
     <message>
         <source>Next</source>
-        <translation type="unfinished">次へ</translation>
+        <translation>次へ</translation>
     </message>
     <message>
         <source>Next image</source>
-        <translation type="unfinished">次の画像</translation>
+        <translation>次の画像</translation>
     </message>
 </context>
 <context>
     <name>design/standard/block/edit</name>
     <message>
         <source>Move up</source>
-        <translation type="unfinished"></translation>
+        <translation>上へ移動</translation>
     </message>
     <message>
         <source>Move down</source>
-        <translation type="unfinished"></translation>
+        <translation>下へ移動</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished">削除</translation>
+        <translation>削除</translation>
     </message>
     <message>
         <source>Are you sure you want to remove this block?</source>
-        <translation type="unfinished"></translation>
+        <translation>このブロックを削除してよろしいですか?</translation>
     </message>
     <message>
         <source>Set overflow</source>
-        <translation type="unfinished"></translation>
+        <translation>オーバーフローを設定</translation>
     </message>
     <message>
         <source>Choose source</source>
-        <translation type="unfinished"></translation>
+        <translation>ソースを選択</translation>
     </message>
     <message>
         <source>d</source>
-        <translation type="unfinished"></translation>
+        <translation>d</translation>
     </message>
     <message>
         <source>h</source>
-        <translation type="unfinished"></translation>
+        <translation>h</translation>
     </message>
     <message>
         <source>m</source>
-        <translation type="unfinished"></translation>
+        <translation>m</translation>
     </message>
     <message>
         <source>s</source>
-        <translation type="unfinished"></translation>
+        <translation>s</translation>
     </message>
     <message>
         <source>left</source>
-        <translation type="unfinished"></translation>
+        <translation>残っています</translation>
     </message>
     <message>
         <source>Queue: no items.</source>
-        <translation type="unfinished"></translation>
+        <translation>公開待ちアイテム: なし</translation>
     </message>
     <message>
         <source>Online: no items.</source>
-        <translation type="unfinished"></translation>
+        <translation>公開済中のアイテム: なし</translation>
     </message>
     <message>
         <source>Rotation:</source>
-        <translation type="unfinished"></translation>
+        <translation>ロテーション: </translation>
     </message>
     <message>
         <source>sec</source>
-        <translation type="unfinished"></translation>
+        <translation>秒</translation>
     </message>
     <message>
         <source>min</source>
-        <translation type="unfinished"></translation>
+        <translation>分</translation>
     </message>
     <message>
         <source>hour</source>
-        <translation type="unfinished"></translation>
+        <translation>時間</translation>
     </message>
     <message>
         <source>day</source>
-        <translation type="unfinished"></translation>
+        <translation>日</translation>
     </message>
     <message>
         <source>Shuffle</source>
-        <translation type="unfinished"></translation>
+        <translation>シャッフル</translation>
     </message>
     <message>
         <source>Set</source>
-        <translation type="unfinished"></translation>
+        <translation>設定</translation>
     </message>
     <message>
         <source>Moved to:</source>
-        <translation type="unfinished"></translation>
+        <translation>移動先: </translation>
     </message>
     <message>
         <source>Not visible</source>
-        <translation type="unfinished"></translation>
+        <translation>非表示</translation>
     </message>
     <message>
         <source>History: no items.</source>
-        <translation type="unfinished"></translation>
+        <translation>履歴: アイテムなし.</translation>
     </message>
     <message>
         <source>Remove selected</source>
-        <translation type="unfinished"></translation>
+        <translation>選択した項目の削除</translation>
     </message>
     <message>
         <source>Queue:</source>
-        <translation type="unfinished"></translation>
+        <translation>公開待ちアイテム: </translation>
     </message>
     <message>
         <source>Online:</source>
-        <translation type="unfinished"></translation>
+        <translation>公開中のアイテム: </translation>
     </message>
     <message>
         <source>History:</source>
-        <translation type="unfinished"></translation>
+        <translation>履歴: </translation>
     </message>
 </context>
 <context>
     <name>design/standard/block/view</name>
     <message>
         <source>Block:</source>
-        <translation type="unfinished"></translation>
+        <translation>ブロック: </translation>
     </message>
 </context>
 <context>
     <name>design/standard/class/datatype</name>
     <message>
         <source>Default layout:</source>
-        <translation type="unfinished"></translation>
+        <translation>デフォルトのレイアウト: </translation>
     </message>
     <message>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>なし</translation>
     </message>
 </context>
 <context>
     <name>design/standard/datatype/ezpage</name>
     <message>
         <source>The total number of zones in the new layout is less than the number of zones in the previous layout. Therefore, you must map the previous zones to new zones. Unmapped zones will be removed!</source>
-        <translation type="unfinished"></translation>
+        <translation>新しいレイアウトには、以前のレイアウトより少ない数のゾーンが設定されています。以前のゾーンを新しいゾーンへ割り当てて下さい。割り当てられていないゾーンは削除されます。!</translation>
     </message>
     <message>
         <source>Set layout</source>
-        <translation type="unfinished"></translation>
+        <translation>レイアウトを設定</translation>
     </message>
     <message>
         <source>Block name:</source>
-        <translation type="unfinished"></translation>
+        <translation>ブッロク名: </translation>
     </message>
     <message>
         <source>Block type:</source>
-        <translation type="unfinished"></translation>
+        <translation>ブロックタイプ: </translation>
     </message>
     <message>
         <source>Add block</source>
-        <translation type="unfinished"></translation>
+        <translation>ブロックを追加</translation>
     </message>
     <message>
         <source>Expand</source>
-        <translation type="unfinished"></translation>
+        <translation>開く</translation>
     </message>
     <message>
         <source>Collapse</source>
-        <translation type="unfinished"></translation>
+        <translation>閉じる</translation>
     </message>
 </context>
 </TS>
