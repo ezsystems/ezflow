e0395010fc618042f3a02f12dccc57730e4452b9
Lukasz Serwatka <ls@ez.no>
2009-09-08 22:06:03 +0000

    - Added ger-DE translation
    git-svn-id: http://svn.ez.no/svn/extensions/ezflow/trunk@4150 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ packages/ezflow_extension/ezextension/ezflow/translations/ger-DE/translation.ts
@@ -0,0 +1,514 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="en">
+<context>
+    <name>design/admin/content/edit</name>
+    <message>
+        <source>Quick search</source>
+        <translation>Schnell Suche</translation>
+    </message>
+    <message>
+        <source>Search phrase</source>
+        <translation>Such Phrase</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Suche</translation>
+    </message>
+    <message>
+        <source>Section to search</source>
+        <translation>Sektion, die durchsucht werden soll</translation>
+    </message>
+    <message>
+        <source>Date range</source>
+        <translation>Datums Bereich</translation>
+    </message>
+    <message>
+        <source>Past day</source>
+        <translation>Letzter Tag</translation>
+    </message>
+    <message>
+        <source>Past week</source>
+        <translation>Letzte Woche</translation>
+    </message>
+    <message>
+        <source>Past month</source>
+        <translation>Letzter Monat</translation>
+    </message>
+    <message>
+        <source>Past 3 months</source>
+        <translation>Letze 3 Monate</translation>
+    </message>
+    <message>
+        <source>Past year</source>
+        <translation>Letztes Jahr</translation>
+    </message>
+    <message>
+        <source>Select block</source>
+        <translation>Block auswählen</translation>
+    </message>
+    <message>
+        <source>Select:</source>
+        <translation>Auswählen:</translation>
+    </message>
+    <message>
+        <source>Add to block</source>
+        <translation>Zu Block hinzufügen</translation>
+    </message>
+    <message>
+        <source>Edit &lt;%object_name&gt; [%class_name]</source>
+        <translation>&lt;%object_name&gt; [%class_name] bearbeiten</translation>
+    </message>
+    <message>
+        <source>Translating content from %from_lang to %to_lang</source>
+        <translation>Übersetze Content von %from_lang nach %to_lang</translation>
+    </message>
+    <message>
+        <source>Back to edit</source>
+        <translation>Zurück zur Bearbeitung</translation>
+    </message>
+    <message>
+        <source>Send for publishing</source>
+        <translation>Zur Veröffentlichung schicken</translation>
+    </message>
+    <message>
+        <source>Publish the contents of the draft that is being edited. The draft will become the published version of the object.</source>
+        <translation>Den Inhalt des derzeitigen Entwurfs veröffentlichen. Damit wird dieser Entwurf die veröffentlichte Versoin dieses Objekt.</translation>
+    </message>
+    <message>
+        <source>Store draft</source>
+        <translation>Entwurf speichern</translation>
+    </message>
+    <message>
+        <source>Store the contents of the draft that is being edited and continue editing. Use this button to periodically save your work while editing.</source>
+        <translation>Den Inhalt des derzeitgen Entwurf speichern und mit der Bearbeitung fortfahren. Benutzen Sie diese Schaltfläche regelmäßig, um Ihre Arbeit zu speichern.</translation>
+    </message>
+    <message>
+        <source>Discard draft</source>
+        <translation>Entwurf verwerfen</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to discard the draft?</source>
+        <translation>Sind Sie sicher, dass Sie den Entwurf verwerfen wollen?</translation>
+    </message>
+    <message>
+        <source>Discard the draft that is being edited. This will also remove the translations that belong to the draft (if any).</source>
+        <translation>Den aktuellen Entwurf verwerfen. Dies wird auch die zum Entwurf gehörende Übersetzungen löschen, wenn welche vorhanden sind.</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin/contextsubmenu</name>
+    <message>
+        <source>Push to block</source>
+        <translation>Zu Block hinzufügen</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin/node/contextmenu</name>
+    <message>
+        <source>eZ Flow</source>
+        <translation>eZ Flow</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin/node/subitemcontextmenu</name>
+    <message>
+        <source>eZ Flow</source>
+        <translation>eZ Flow</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin/page/push</name>
+    <message>
+        <source>Choose placement for &quot;%node_name&quot;</source>
+        <translation>Ort für &quot;%node_name&quot; auswählen</translation>
+    </message>
+    <message>
+        <source>Select frontpage</source>
+        <translation>Frontpage auswählen</translation>
+    </message>
+    <message>
+        <source>Select zone</source>
+        <translation>Zone auswählen</translation>
+    </message>
+    <message>
+        <source>Select block</source>
+        <translation>Block auswählen</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Hinzufügen</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Entfernen</translation>
+    </message>
+    <message>
+        <source>Store</source>
+        <translation>Speichern</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezflow/block/online_users</name>
+    <message>
+        <source>There are currently %logged_in_count active users on the system.</source>
+        <translation>Derzeit sind %logged_in_count aktive Benutzer im System.</translation>
+    </message>
+    <message>
+        <source>There are %anonymous_count anonymous users accessing the site.</source>
+        <translation>Derzeit sind %anonymous_count anonyme Benutzer auf der Site.</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezflow/edit/frontpage</name>
+    <message>
+        <source>Search phrase</source>
+        <translation>Such Phrase</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Suche</translation>
+    </message>
+    <message>
+        <source>Section to search</source>
+        <translation>Sektion, die durchsucht werden soll</translation>
+    </message>
+    <message>
+        <source>Date range</source>
+        <translation>Datums Bereich</translation>
+    </message>
+    <message>
+        <source>Past day</source>
+        <translation>Letzter Tag</translation>
+    </message>
+    <message>
+        <source>Past week</source>
+        <translation>Letzte Woche</translation>
+    </message>
+    <message>
+        <source>Past month</source>
+        <translation>Letzter Monat</translation>
+    </message>
+    <message>
+        <source>Past 3 months</source>
+        <translation>Letze 3 Monate</translation>
+    </message>
+    <message>
+        <source>Past year</source>
+        <translation>Letztes Jahr</translation>
+    </message>
+    <message>
+        <source>Results</source>
+        <translation>Ergebnisse</translation>
+    </message>
+    <message>
+        <source>Select:</source>
+        <translation>Auswählen:</translation>
+    </message>
+    <message>
+        <source>Add to block</source>
+        <translation>Zu Block hinzufügen</translation>
+    </message>
+    <message>
+        <source>Edit %1 - %2</source>
+        <translation>%1 - %2 bearbeiten</translation>
+    </message>
+    <message>
+        <source>Translating content from %from_lang to %to_lang</source>
+        <translation>Übersetze Content von %from_lang nach %to_lang</translation>
+    </message>
+    <message>
+        <source>Content in %language</source>
+        <translation>Content in %language</translation>
+    </message>
+    <message>
+        <source>Send for publishing</source>
+        <translation>Zur Veröffentlichung schicken</translation>
+    </message>
+    <message>
+        <source>Store draft</source>
+        <translation>Entwurf speichern</translation>
+    </message>
+    <message>
+        <source>Discard draft</source>
+        <translation>Entwurf verwerfen</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezflow/embed/poll</name>
+    <message>
+        <source>Vote</source>
+        <translation>Wählen</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezflow/pagelayout</name>
+    <message>
+        <source>Skip to main content</source>
+        <translation>Bei Hauptinhalt fortfahren</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezflow/parts/website_toolbar</name>
+    <message>
+        <source>Push to block</source>
+        <translation>Zu Block hinzufügen</translation>
+    </message>
+    <message>
+        <source>Timeline</source>
+        <translation>Zeitachse</translation>
+    </message>
+</context>
+<context>
+    <name>design/iphone/collectedinfo/poll</name>
+    <message>
+        <source>Poll %pollname</source>
+        <translation>%pollname Umfrage</translation>
+    </message>
+    <message>
+        <source>Results</source>
+        <translation>Ergebnisse</translation>
+    </message>
+    <message>
+        <source>Please log in to vote on this poll.</source>
+        <translation>Bitte loggen Sie sich, um bei der Umfrage wählen zu können.</translation>
+    </message>
+    <message>
+        <source>You have already voted for this poll.</source>
+        <translation>Sie haben bereits Ihre Stimme bei dieser Umfrage abgegeben.</translation>
+    </message>
+    <message>
+        <source>Votes</source>
+        <translation>Stimmen</translation>
+    </message>
+    <message>
+        <source>%count total votes</source>
+        <translation>%count Gesamtstimmen</translation>
+    </message>
+    <message>
+        <source>Back to poll</source>
+        <translation>Zurück zur Umfrage</translation>
+    </message>
+</context>
+<context>
+    <name>design/iphone/embed/poll</name>
+    <message>
+        <source>Vote</source>
+        <translation>Umfrage</translation>
+    </message>
+</context>
+<context>
+    <name>design/iphone/full/image</name>
+    <message>
+        <source>Previous</source>
+        <translation>Zurück</translation>
+    </message>
+    <message>
+        <source>Next</source>
+        <translation>Weiter</translation>
+    </message>
+    <message>
+        <source>Next image</source>
+        <translation>Nächstes Bild</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/block/edit</name>
+    <message>
+        <source>Move up</source>
+        <translation>Nach oben</translation>
+    </message>
+    <message>
+        <source>Move down</source>
+        <translation>Nach unten</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Entfernen</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to remove this block?</source>
+        <translation>Sind Sie sicher, dass Sie diesen Block entfernen wollen?</translation>
+    </message>
+    <message>
+        <source>Set overflow</source>
+        <translation>Überlauf setzen</translation>
+    </message>
+    <message>
+        <source>Choose source</source>
+        <translation>Quelle wählen</translation>
+    </message>
+    <message>
+        <source>d</source>
+        <translation>t</translation>
+    </message>
+    <message>
+        <source>h</source>
+        <translation>h</translation>
+    </message>
+    <message>
+        <source>m</source>
+        <translation>m</translation>
+    </message>
+    <message>
+        <source>s</source>
+        <translation>s</translation>
+    </message>
+    <message>
+        <source>left</source>
+        <translation>links</translation>
+    </message>
+    <message>
+        <source>Queue: no items.</source>
+        <translation>Warteschlange: Keine Einträge.</translation>
+    </message>
+    <message>
+        <source>Online: no items.</source>
+        <translation>Online: Keine Einträge.</translation>
+    </message>
+    <message>
+        <source>Rotation:</source>
+        <translation>Rotation:</translation>
+    </message>
+    <message>
+        <source>min</source>
+        <translation>min</translation>
+    </message>
+    <message>
+        <source>hour</source>
+        <translation>Stunde</translation>
+    </message>
+    <message>
+        <source>day</source>
+        <translation>Tag</translation>
+    </message>
+    <message>
+        <source>Shuffle</source>
+        <translation>Mischen</translation>
+    </message>
+    <message>
+        <source>Set</source>
+        <translation>Setzen</translation>
+    </message>
+    <message>
+        <source>Moved to:</source>
+        <translation>Verschoben nach:</translation>
+    </message>
+    <message>
+        <source>Not visible</source>
+        <translation>Nicht sichtbar</translation>
+    </message>
+    <message>
+        <source>History: no items.</source>
+        <translation>Geschichte: Keine Einträge.</translation>
+    </message>
+    <message>
+        <source>Remove selected</source>
+        <translation>Ausgewähltes entfernen</translation>
+    </message>
+    <message>
+        <source>Queue:</source>
+        <translation>Warteschlange:</translation>
+    </message>
+    <message>
+        <source>Online:</source>
+        <translation>Online:</translation>
+    </message>
+    <message>
+        <source>History:</source>
+        <translation>Geschichte:</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/block/view</name>
+    <message>
+        <source>Block:</source>
+        <translation>Block:</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/class/datatype</name>
+    <message>
+        <source>Default layout:</source>
+        <translation>Standard Layout:</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>Keines</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/datatype/ezpage</name>
+    <message>
+        <source>The total number of zones in the new layout is less than the number of zones in the previous layout. Therefore, you must map the previous zones to new zones. Unmapped zones will be removed!</source>
+        <translation>Die Gesamtanzahl der Zonen im neuen Layout ist kleiner als die im alten Layout. Sie müssen die vorigen Zonen den neuen zuordnen. Nicht zugeordnete Zonen werden gelöscht!</translation>
+    </message>
+    <message>
+        <source>Set layout</source>
+        <translation>Layout einstellen</translation>
+    </message>
+    <message>
+        <source>Block name:</source>
+        <translation>Block Name:</translation>
+    </message>
+    <message>
+        <source>Block type:</source>
+        <translation>Block Typ:</translation>
+    </message>
+    <message>
+        <source>Add block</source>
+        <translation>Block hinzufügen</translation>
+    </message>
+    <message>
+        <source>Expand</source>
+        <translation>Aufklappen</translation>
+    </message>
+    <message>
+        <source>Collapse</source>
+        <translation>Zuklappen</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/page/push</name>
+    <message>
+        <source>Choose placement for &quot;%node_name&quot;</source>
+        <translation>Ort für &quot;%node_name&quot; auswählen</translation>
+    </message>
+    <message>
+        <source>Select frontpage</source>
+        <translation>Frontpage auswählen</translation>
+    </message>
+    <message>
+        <source>Select zone</source>
+        <translation>Zone auswählen</translation>
+    </message>
+    <message>
+        <source>Select block</source>
+        <translation>Block auswählen</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Hinzufügen</translation>
+    </message>
+    <message>
+        <source>Placement list</source>
+        <translation>Platzierungs Liste</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Entfernen</translation>
+    </message>
+    <message>
+        <source>Store</source>
+        <translation>Speichern</translation>
+    </message>
+</context>
+<context>
+    <name>ezflow/push</name>
+    <message>
+        <location filename="push.php" line="49"/>
+        <source>Push to block</source>
+        <translation>Zu Block hinzufügen</translation>
+    </message>
+</context>
+</TS>
