1144f632b5b699ef6ca96e7d17a569b6c4cea3ca
André Rømcke <ar@ez.no>
2010-02-15 16:48:35 +0000

    cs
    git-svn-id: http://svn.ez.no/svn/extensions/ezflow/trunk@4429 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezflow_extension/ezextension/ezflow/design/standard/javascript/ezajaxsearch.js
+++ packages/ezflow_extension/ezextension/ezflow/design/standard/javascript/ezajaxsearch.js
@@ -1,21 +1,27 @@
-var eZAJAXSearch = function() {
+var eZAJAXSearch = function()
+{
     var ret = {};
-    
-    var yCallback = function(Y, result) {
-        var successCallBack = function(id, o) {
-            if (o.responseJSON !== undefined) {
+
+    var yCallback = function(Y, result)
+    {
+        var successCallBack = function(id, o)
+        {
+            if (o.responseJSON !== undefined)
+            {
                 var response = o.responseJSON;
 
-                if (response.content.SearchResult !== undefined) {
+                if (response.content.SearchResult !== undefined)
+                {
                     var itemCount = response.content.SearchResult.length;
 
                     var resultsTarget = Y.get(ret.cfg.searchresults);
                     resultsTarget.set('innerHTML', '');
                     resultsTarget.addClass('loading');
 
-                    for(var i = 0; i < itemCount; i++) {
+                    for(var i = 0; i < itemCount; i++)
+                    {
                         var item = response.content.SearchResult[i];
-                        
+
                         var template = ret.cfg.resulttemplate;
                         template = template.replace(/\{+title+\}/, item.name);
                         template = template.replace(/\{+date+\}/, item.published_date);
@@ -32,46 +38,55 @@ var eZAJAXSearch = function() {
             }
         }
 
-        var getValueForSelector = function(sel) {
+        var getValueForSelector = function(sel)
+        {
             var value, node = Y.one(sel);
-            
-            if ( node ) {
+
+            if ( node )
+            {
                 if (node.get('nodeName').toLowerCase() == 'input'
                         && (node.get('type') == 'radio' 
-                                || node.get('type') == 'checkbox')) {
+                                || node.get('type') == 'checkbox'))
+                {
                     value = (Y.one(sel + ':checked') != null) ? Y.one(sel + ':checked').get('value') : null;
-                } else if (node.get('nodeName').toLowerCase() == 'select'
-                                && node.hasAttribute('multiple')) {
+                }
+                else if (node.get('nodeName').toLowerCase() == 'select'
+                                && node.hasAttribute('multiple'))
+                {
                     value = [];
-                
-                    for (var i = 0; i < node.get('options').size(); i++) {
+
+                    for (var i = 0; i < node.get('options').size(); i++)
+                    {
                         var option = node.get('options').item(i);
                         if (option.get('selected'))
                             value.push(option.get('value'));
                     }
 
                     value = value.join(',');
-                } else {
+                }
+                else
+                {
                     value = node.get('value');
                 }
             }
-            
+
             return value;
         }
 
-        var performSearch = function() {
+        var performSearch = function()
+        {
             var searchString = getValueForSelector(ret.cfg.searchstring);
             var dateFormatType = ret.cfg.dateformattype;
 
             var value, data = 'SearchStr=' + searchString;
             data += '&SearchLimit=' + getValueForSelector('[name=SearchLimit]');
-            
+
             if (value = getValueForSelector('[name=SearchOffset]'))
                 data += '&SearchOffset=' + value;
-            
+
             if (value = getValueForSelector('[name=SearchSectionID]'))
                 data += '&SearchSectionID=' + value;
-            
+
             if (value = getValueForSelector('[name=SearchDate]'))
                 data += '&SearchDate=' +  value;
 
@@ -91,23 +106,26 @@ var eZAJAXSearch = function() {
                 data += '&SearchTimestamp=' + value;
 
             data += '&EncodingFormatDate=' + dateFormatType;
-            
+
             var backendUri = ret.cfg.backendUri ? ret.cfg.backendUri : 'ezjsc::search' ;
-            
-            if(searchString !== '') {
+
+            if(searchString !== '')
+            {
                 Y.io.ez(backendUri, {on: {success: successCallBack}, method: 'POST', data: data });
             }
         }
 
-        var handleClick = function(e) {
+        var handleClick = function(e)
+        {
             performSearch();
-
             e.preventDefault();
         }
-        
-        var handleKeyPress = function(e) {
+
+        var handleKeyPress = function(e)
+        {
             var key = e.which || e.keyCode;
-            if (key == 13) {
+            if (key == 13)
+            {
                 performSearch();
                 e.halt();
             }
@@ -118,7 +136,8 @@ var eZAJAXSearch = function() {
     }
     ret.cfg = {};
 
-    ret.init = function() {
+    ret.init = function()
+    {
         var ins = YUI(YUI3_config).use('node', 'event', 'io-ez', yCallback);
     }
     
