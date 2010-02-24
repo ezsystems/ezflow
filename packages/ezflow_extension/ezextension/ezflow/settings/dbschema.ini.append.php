caaa9d6bc0e3b19089a31d034ec3f0835e5e9385
Gaetano Giunta <gg@ez.no>
2010-02-24 22:48:26 +0000

    - Fixed issue #016264: bad index naming in oracle
    git-svn-id: http://svn.ez.no/svn/extensions/ezflow/trunk@4443 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ packages/ezflow_extension/ezextension/ezflow/settings/dbschema.ini.append.php
@@ -0,0 +1,8 @@
+<?php /*
+
+/* shorten index names over 30 chars  for oracle */
+
+[oracle]
+IndexNameTranslation[ezm_pool.ezm_pool__block_id__ts_publication__priority]=ezm_pool_block_id_ts_publ_prio
+
+*/ ?>
