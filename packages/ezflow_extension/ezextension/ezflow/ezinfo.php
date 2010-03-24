886fe13ae42f4cf411d75b3145080766117cc68d
Mr. Hudson <hudson@ez.no>
2010-03-24 17:07:09 +0000

    - Fixed copyright year
    git-svn-id: http://svn.ez.no/svn/extensions/ezflow/trunk@4550 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezflow_extension/ezextension/ezflow/ezinfo.php
+++ packages/ezflow_extension/ezextension/ezflow/ezinfo.php
@@ -3,7 +3,7 @@
 // ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
 // SOFTWARE NAME: eZ Flow
 // SOFTWARE RELEASE: 1.1-0
-// COPYRIGHT NOTICE: Copyright (C) 1999-2009 eZ Systems AS
+// COPYRIGHT NOTICE: Copyright (C) 1999-2010 eZ Systems AS
 // SOFTWARE LICENSE: GNU General Public License v2.0
 // NOTICE: >
 //   This program is free software; you can redistribute it and/or
@@ -30,7 +30,7 @@ class ezflowInfo
     {
         return array( 'Name' => 'eZ Flow',
                       'Version' => '1.1-0',
-                      'Copyright' => 'Copyright (C) 1999-' . date('Y') . ' eZ Systems AS',
+                      'Copyright' => 'Copyright (C) 1999-2010 eZ Systems AS',
                       'License' => 'GNU General Public License v2.0'
                      );
     }
