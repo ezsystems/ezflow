81f5803dfd142c7978e99e2f32b5ca3f549b0299
Bertrand Dunogier <bd@ez.no>
2011-03-29 17:15:14 +0200

    Fixed license in ezinfo.php

--- packages/ezflow_extension/ezextension/ezflow/ezinfo.php
+++ packages/ezflow_extension/ezextension/ezflow/ezinfo.php
@@ -31,7 +31,7 @@ class ezflowInfo
         return array( 'Name' => 'eZ Flow',
                       'Version' => '//autogentag//',
                       'Copyright' => 'Copyright (C) 1999-2011 eZ Systems AS',
-                      'License' => '//EZP_LICENSE//',
+                      'License' => 'GNU General Public License v2.0',
                      );
     }
 }
