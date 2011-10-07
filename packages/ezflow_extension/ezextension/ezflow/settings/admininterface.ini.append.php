50e0a03c73c0edf2947110619e686a786f9c1aad
Lukasz Serwatka <l.serwatka@gazeta.pl>
2011-10-07 10:09:40 +0200

    Removed example tab

--- packages/ezflow_extension/ezextension/ezflow/settings/admininterface.ini.append.php
+++ packages/ezflow_extension/ezextension/ezflow/settings/admininterface.ini.append.php
@@ -5,12 +5,4 @@ ContextMenuTemplateArray[]=node/ezflcontextmenu.tpl
 SubitemsContextMenuTemplateArray[]=node/ezflsubitemscontextmenu.tpl
 SubMenuTemplateArray[]=node/ezflcontextsubmenu.tpl
 
-[WindowControlsSettings]
-AdditionalTabs[]=ezflow
-
-[AdditionalTab_ezflow]
-Title=eZ Flow
-Description=Information about object relation to eZ Flow datatype
-Template=ezflow.tpl
-
 */ ?>
