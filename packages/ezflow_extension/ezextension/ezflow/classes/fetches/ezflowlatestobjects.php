6601165e2023b02c4039c508c2f26a06e1750406
Lukasz Serwatka <l.serwatka@gazeta.pl>
2011-10-12 14:11:12 +0200

    Fixed #018097: New ezflow block don't sort articles (cherry picked from commit 43bf8700110bb4a7f0ab5779f7b619631aff5548)

--- packages/ezflow_extension/ezextension/ezflow/classes/fetches/ezflowlatestobjects.php
+++ packages/ezflow_extension/ezextension/ezflow/classes/fetches/ezflowlatestobjects.php
@@ -44,7 +44,7 @@ class eZFlowLatestObjects implements eZFlowFetchInterface
 
         $subTreeParameters = array();
         $subTreeParameters['AsObject'] = false;
-        $subTreeParameters['SortBy'] = array( 'published', true ); // first the oldest
+        $subTreeParameters['SortBy'] = array( 'published', false ); // first the latest
         $subTreeParameters['AttributeFilter'] = array(
             'and',
             array( 'published', '>', $publishedAfter ),
