50ce91c958fbe813512346b6cb762097752cf309
Damien Pobel <dp@ez.no>
2012-03-29 13:58:20 +0200

    Changed: welcome 2012

--- packages/ezflow_extension/ezextension/ezflow/ezinfo.php
+++ packages/ezflow_extension/ezextension/ezflow/ezinfo.php
@@ -3,7 +3,7 @@
 // ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
 // SOFTWARE NAME: eZ Flow
 // SOFTWARE RELEASE: 1.1-0
-// COPYRIGHT NOTICE: Copyright (C) 1999-2011 eZ Systems AS
+// COPYRIGHT NOTICE: Copyright (C) 1999-2012 eZ Systems AS
 // SOFTWARE LICENSE: GNU General Public License v2.0
 // NOTICE: >
 //   This program is free software; you can redistribute it and/or
@@ -30,7 +30,7 @@ class ezflowInfo
     {
         return array( 'Name' => 'eZ Flow',
                       'Version' => '//autogentag//',
-                      'Copyright' => 'Copyright (C) 1999-2011 eZ Systems AS',
+                      'Copyright' => 'Copyright (C) 1999-2012 eZ Systems AS',
                       'License' => 'GNU General Public License v2.0',
                      );
     }
