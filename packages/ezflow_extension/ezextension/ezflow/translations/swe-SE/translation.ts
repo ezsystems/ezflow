c5359d7536c5c3ba2afbb1bace450d564c2a0791
Leif-Jöran Olsson <ljo@fripost.org>
2012-04-04 21:28:35 +0200

    Added Swedish, swe-SE, translation for ezflow.

---
+++ packages/ezflow_extension/ezextension/ezflow/translations/swe-SE/translation.ts
@@ -0,0 +1,567 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sv_SE">
+<context>
+    <name>design/admin/content/edit</name>
+    <message>
+        <source>Quick search</source>
+        <translation>Snabbsökning</translation>
+    </message>
+    <message>
+        <source>Search phrase</source>
+        <translation>Sökuttryck</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+    <message>
+        <source>Section to search</source>
+        <translation>Avdelning att söka i</translation>
+    </message>
+    <message>
+        <source>Date range</source>
+        <translation>Datumintervall</translation>
+    </message>
+    <message>
+        <source>Past day</source>
+        <translation>Senaste dygnet</translation>
+    </message>
+    <message>
+        <source>Past week</source>
+        <translation>Senaste veckan</translation>
+    </message>
+    <message>
+        <source>Past month</source>
+        <translation>Senaste månaden</translation>
+    </message>
+    <message>
+        <source>Past 3 months</source>
+        <translation>Senaste kvartalet</translation>
+    </message>
+    <message>
+        <source>Past year</source>
+        <translation>Senaste året</translation>
+    </message>
+    <message>
+        <source>Select block</source>
+        <translation>Välj block</translation>
+    </message>
+    <message>
+        <source>Select:</source>
+        <translation>Välj:</translation>
+    </message>
+    <message>
+        <source>Add to block</source>
+        <translation>Lägg till i block</translation>
+    </message>
+    <message>
+        <source>Edit &lt;%object_name&gt; [%class_name]</source>
+        <translation>Redigera &lt;%object_name&gt; [%class_name]</translation>
+    </message>
+    <message>
+        <source>Translating content from %from_lang to %to_lang</source>
+        <translation>Översätter innehåll från %from_lang till %to_lang</translation>
+    </message>
+    <message>
+        <source>Back to edit</source>
+        <translation>Tillbaka till redigera</translation>
+    </message>
+    <message>
+        <source>Send for publishing</source>
+        <translation>Skicka för publicering</translation>
+    </message>
+    <message>
+        <source>Publish the contents of the draft that is being edited. The draft will become the published version of the object.</source>
+        <translation>Publicera innehållet i aktuellt utkast. Utkastet blir den nya publicerade versionen av objektet.</translation>
+    </message>
+    <message>
+        <source>Store draft</source>
+        <translation>Spara utkast</translation>
+    </message>
+    <message>
+        <source>Store the contents of the draft that is being edited and continue editing. Use this button to periodically save your work while editing.</source>
+        <translation>Spara innehållet i utkastet och fortsätt redigera det. Använd för att regelbundet spara under redigeringsarbetet.</translation>
+    </message>
+    <message>
+        <source>Discard draft</source>
+        <translation>Kasta utkast</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to discard the draft?</source>
+        <translation>Vill du verkligen kasta utkastet?</translation>
+    </message>
+    <message>
+        <source>Discard the draft that is being edited. This will also remove the translations that belong to the draft (if any).</source>
+        <translation>Kasta det aktuella utkastet. Det innebär att även eventuella översättningar som hör till kastas.</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin/contextsubmenu</name>
+    <message>
+        <source>Add to block in frontpage</source>
+        <translation>Lägg till i block på framsidan</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin/node/contextmenu</name>
+    <message>
+        <source>eZ Flow</source>
+        <translation>eZ Flow</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin/node/subitemcontextmenu</name>
+    <message>
+        <source>eZ Flow</source>
+        <translation>eZ Flow</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin/page/push</name>
+    <message>
+        <source>Choose placement for &quot;%node_name&quot;</source>
+        <translation>Välj plats för &quot;%node_name&quot;</translation>
+    </message>
+    <message>
+        <source>Select frontpage</source>
+        <translation>Välj framsida</translation>
+    </message>
+    <message>
+        <source>Select zone</source>
+        <translation>Välj zon</translation>
+    </message>
+    <message>
+        <source>Select block</source>
+        <translation>Välj block</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Lägg till</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Store</source>
+        <translation>Spara</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezflow/block/online_users</name>
+    <message>
+        <source>There are currently %logged_in_count active users on the system.</source>
+        <translation>Antal aktiva användare i systemet är just nu %logged_in_count.</translation>
+    </message>
+    <message>
+        <source>There are %anonymous_count anonymous users accessing the site.</source>
+        <translation>Antal anonyma användare i systemet är just nu %anonymous_count.</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezflow/block/search</name>
+    <message>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+    <message>
+        <source>No search results found for:</source>
+        <translation>Inga sökresultat för:</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezflow/edit/frontpage</name>
+    <message>
+        <source>Search phrase</source>
+        <translation>Sökfras</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+    <message>
+        <source>Section to search</source>
+        <translation>Avdelning att söka i</translation>
+    </message>
+    <message>
+        <source>Date range</source>
+        <translation>Datumintervall</translation>
+    </message>
+    <message>
+        <source>Past day</source>
+        <translation>Senaste dygnet</translation>
+    </message>
+    <message>
+        <source>Past week</source>
+        <translation>Senaste veckan</translation>
+    </message>
+    <message>
+        <source>Past month</source>
+        <translation>Senaste månaden</translation>
+    </message>
+    <message>
+        <source>Past 3 months</source>
+        <translation>Senaste kvartalet</translation>
+    </message>
+    <message>
+        <source>Past year</source>
+        <translation>Senaste året</translation>
+    </message>
+    <message>
+        <source>Results</source>
+        <translation>Resultat</translation>
+    </message>
+    <message>
+        <source>Select:</source>
+        <translation>Välj:</translation>
+    </message>
+    <message>
+        <source>Add to block</source>
+        <translation>Lägg till i block</translation>
+    </message>
+    <message>
+        <source>Translating content from %from_lang to %to_lang</source>
+        <translation>Översätter innehåll från %from_lang till %to_lang</translation>
+    </message>
+    <message>
+        <source>Content in %language</source>
+        <translation>Innehåll på %language</translation>
+    </message>
+    <message>
+        <source>Send for publishing</source>
+        <translation>Skicka för publicering</translation>
+    </message>
+    <message>
+        <source>Store draft</source>
+        <translation>Spara utkast</translation>
+    </message>
+    <message>
+        <source>Discard draft</source>
+        <translation>Kasta utkast</translation>
+    </message>
+    <message>
+        <source>Edit &lt;%object_name&gt; (%class_name)</source>
+        <translation>Redigera &lt;%object_name&gt; (%class_name)</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezflow/embed/poll</name>
+    <message>
+        <source>Vote</source>
+        <translation>Rösta</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezflow/pagelayout</name>
+    <message>
+        <source>Skip to main content</source>
+        <translation>Hoppas till huvudinnehåll</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezflow/parts/website_toolbar</name>
+    <message>
+        <source>Timeline</source>
+        <translation>Tidslinje</translation>
+    </message>
+    <message>
+        <source>Add to block in frontpage</source>
+        <translation>Lägg till i block på framsidan</translation>
+    </message>
+</context>
+<context>
+    <name>design/iphone/collectedinfo/poll</name>
+    <message>
+        <source>Results</source>
+        <translation>Resultat</translation>
+    </message>
+    <message>
+        <source>Please log in to vote on this poll.</source>
+        <translation>Logga in för att rösta i undersökningen.</translation>
+    </message>
+    <message>
+        <source>You have already voted for this poll.</source>
+        <translation>Du verkar redan ha röstat i den här undersökningen. </translation>
+    </message>
+    <message>
+        <source>Votes</source>
+        <translation>Röster</translation>
+    </message>
+    <message>
+        <source>%count total votes</source>
+        <translation>%count av rösterna</translation>
+    </message>
+    <message>
+        <source>Back to poll</source>
+        <translation>Tillbaka till undersökningen</translation>
+    </message>
+    <message>
+        <source>Poll %pollname</source>
+        <translation>Undersökning %pollname</translation>
+    </message>
+</context>
+<context>
+    <name>design/iphone/embed/poll</name>
+    <message>
+        <source>Vote</source>
+        <translation>Rösta</translation>
+    </message>
+</context>
+<context>
+    <name>design/iphone/full/image</name>
+    <message>
+        <source>Previous</source>
+        <translation>Föregående</translation>
+    </message>
+    <message>
+        <source>Next</source>
+        <translation>Nästa</translation>
+    </message>
+    <message>
+        <source>Next image</source>
+        <translation>Nästa bild</translation>
+    </message>
+</context>
+<context>
+    <name>design/iphone/page_footer</name>
+    <message>
+        <source>Visit:</source>
+        <translation>Besök:</translation>
+    </message>
+    <message>
+        <source>mobile site</source>
+        <translation>mobil webbplats</translation>
+    </message>
+    <message>
+        <source>full site</source>
+        <translation>Full webbplats</translation>
+    </message>
+</context>
+<context>
+    <name>design/iphone/pagelayout</name>
+    <message>
+        <source>Navigation</source>
+        <translation>Navigering</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/block/edit</name>
+    <message>
+        <source>Move up</source>
+        <translation>Flytta upp</translation>
+    </message>
+    <message>
+        <source>Move down</source>
+        <translation>Flytta ner</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>ta bort</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to remove this block?</source>
+        <translation>Vill du verkligen ta bort blocket?</translation>
+    </message>
+    <message>
+        <source>Set overflow</source>
+        <translation>Ställ in flödning</translation>
+    </message>
+    <message>
+        <source>Choose source</source>
+        <translation>Välj källa</translation>
+    </message>
+    <message>
+        <source>d</source>
+        <translation>d</translation>
+    </message>
+    <message>
+        <source>h</source>
+        <translation>t</translation>
+    </message>
+    <message>
+        <source>m</source>
+        <translation>m</translation>
+    </message>
+    <message>
+        <source>s</source>
+        <translatorcomment>för lite kontext</translatorcomment>
+        <translation>s</translation>
+    </message>
+    <message>
+        <source>left</source>
+        <translation>vänster</translation>
+    </message>
+    <message>
+        <source>Queue: no items.</source>
+        <translation>Kö: inga enheter.</translation>
+    </message>
+    <message>
+        <source>Online: no items.</source>
+        <translation>Online: inga enheter.</translation>
+    </message>
+    <message>
+        <source>Rotation:</source>
+        <translation>Rotation:</translation>
+    </message>
+    <message>
+        <source>min</source>
+        <translation>min</translation>
+    </message>
+    <message>
+        <source>hour</source>
+        <translatorcomment>för lite kontext</translatorcomment>
+        <translation>timme</translation>
+    </message>
+    <message>
+        <source>day</source>
+        <translation>dag</translation>
+    </message>
+    <message>
+        <source>Shuffle</source>
+        <translation>Kasta om</translation>
+    </message>
+    <message>
+        <source>Set</source>
+        <translation>Ställ in</translation>
+    </message>
+    <message>
+        <source>Moved to:</source>
+        <translation>Flyttad till:</translation>
+    </message>
+    <message>
+        <source>Not visible</source>
+        <translation>Ej synlig</translation>
+    </message>
+    <message>
+        <source>History: no items.</source>
+        <translation>Historik: inga enheter.</translation>
+    </message>
+    <message>
+        <source>Remove selected</source>
+        <translation>Ta bort valda</translation>
+    </message>
+    <message>
+        <source>Queue:</source>
+        <translation>Kö:</translation>
+    </message>
+    <message>
+        <source>Online:</source>
+        <translation>Online:</translation>
+    </message>
+    <message>
+        <source>History:</source>
+        <translation>Historik:</translation>
+    </message>
+    <message>
+        <source>Add item</source>
+        <translation>Lägg till</translation>
+    </message>
+    <message>
+        <source>Rotating item.</source>
+        <translation>Roterande enhet.</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Namn:</translation>
+    </message>
+    <message>
+        <source>Current source:</source>
+        <translation>Aktuell källa:</translation>
+    </message>
+    <message>
+        <source>Publishing schedule for: </source>
+        <translation>Publiceringsschema för:</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/block/view</name>
+    <message>
+        <source>Block:</source>
+        <translation>Block:</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/class/datatype</name>
+    <message>
+        <source>Default layout:</source>
+        <translation>Skönslayout:</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>Inga</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/datatype/ezpage</name>
+    <message>
+        <source>The total number of zones in the new layout is less than the number of zones in the previous layout. Therefore, you must map the previous zones to new zones. Unmapped zones will be removed!</source>
+        <translation>Det totala antalet zoner i den nya layouten är mindre än antalet i föregående layot. Du måste därför koppla föregående zoner till de nya. Zoner som inte kopplas tas bort!</translation>
+    </message>
+    <message>
+        <source>Set layout</source>
+        <translation>Ställ in layout</translation>
+    </message>
+    <message>
+        <source>Add block</source>
+        <translation>Lägg till block</translation>
+    </message>
+    <message>
+        <source>Expand All</source>
+        <translation>Expandera alla</translation>
+    </message>
+    <message>
+        <source>Collapse All</source>
+        <translation>Dölj alla detaljer</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/page/push</name>
+    <message>
+        <source>Choose placement for &quot;%node_name&quot;</source>
+        <translation>Välj plats för &quot;%node_name&quot;</translation>
+    </message>
+    <message>
+        <source>Select frontpage</source>
+        <translation>Välj framsida</translation>
+    </message>
+    <message>
+        <source>Select zone</source>
+        <translation>Välj zon</translation>
+    </message>
+    <message>
+        <source>Select block</source>
+        <translation>Välj block</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Lägg till</translation>
+    </message>
+    <message>
+        <source>Placement list</source>
+        <translation>Platslistning</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Store</source>
+        <translation>Spara</translation>
+    </message>
+</context>
+<context>
+    <name>ezflow/push</name>
+    <message>
+        <source>Push to block</source>
+        <translation>Flytta till block</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/workflow/event</name>
+    <message>
+        <source>eZ Page swap workflow event</source>
+        <translation type="unfinished">Arbetsflödeshändelse  för eZ-sidomkastning</translation>
+    </message>
+</context>
+</TS>
